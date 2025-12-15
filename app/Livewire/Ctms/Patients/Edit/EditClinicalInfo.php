37bf9b68b83ecc2816d60a26bb3ab28dea739ac6
mvksastry <mvksastry@gmail.com>
2025-12-15 21:29:51 +0530

    edited data entries for clinical data entry

--- app/Livewire/Ctms/Patients/Edit/EditClinicalInfo.php
+++ app/Livewire/Ctms/Patients/Edit/EditClinicalInfo.php
@@ -32,6 +32,28 @@ class EditClinicalInfo extends Component
         $this->form->opd_id = ($clinical_info != null) ? $clinical_info->opd_id : "";
         $this->form->in_patient_id = ($clinical_info != null) ? $clinical_info->in_patient_id : "";
         $this->form->admission_date = ($clinical_info != null) ? $clinical_info->admission_date : "";
+
+        $this->form->oande = ($clinical_info != null) ? $clinical_info->o_e : "";
+        $this->form->pr = ($clinical_info != null) ? $clinical_info->pr : "";
+        $this->form->temperature = ($clinical_info != null) ? $clinical_info->temperature : "";
+
+        $this->form->bp_systolic = ($clinical_info != null) ? $clinical_info->bp_systolic : "";
+        $this->form->bp_diastolic = ($clinical_info != null) ? $clinical_info->bp_diastolic : "";
+        $this->form->cvs = ($clinical_info != null) ? $clinical_info->cvs : "";
+        $this->form->panda = ($clinical_info != null) ? $clinical_info->p_a : "";
+        $this->form->cns = ($clinical_info != null) ? $clinical_info->cns : "";
+        $this->form->cbc = ($clinical_info != null) ? $clinical_info->cbc : "";
+        $this->form->esr = ($clinical_info != null) ? $clinical_info->esr : "";
+        $this->form->crp = ($clinical_info != null) ? $clinical_info->crp : "";
+        $this->form->rft = ($clinical_info != null) ? $clinical_info->rft : "";
+        $this->form->lft = ($clinical_info != null) ? $clinical_info->lft : "";
+        
+        $this->form->clotting_time = ($clinical_info != null) ? $clinical_info->clotting_time : "";
+        $this->form->bleeding_time = ($clinical_info != null) ? $clinical_info->bleeding_time : "";
+        $this->form->prothrombin_time = ($clinical_info != null) ? $clinical_info->prothrombin_time : "";
+        $this->form->procalcitonin = ($clinical_info != null) ? $clinical_info->procalcitonin : "";
+        $this->form->laboratory_report_file = ($clinical_info != null) ? $clinical_info->laboratory_report_file : "";
+        
     }
 
     public function fnSaveEditedClinicalData()
