be6cf3f28c771a4dcbd74f64dc19b83eb64eb2ad
mvksastry <mvksastry@gmail.com>
2025-12-19 05:35:27 +0530

    clinical inform form fields made nullable

--- app/Livewire/Forms/PatientCIForm.php
+++ app/Livewire/Forms/PatientCIForm.php
@@ -17,58 +17,58 @@ class PatientCIForm extends Form
     #[Validate('nullable|date')]
     public $admission_date = null;
 
-    #[Validate('regex:/^[A-Za-z0-9 ]+$/')]
+    #[Validate('nullable|regex:/^[A-Za-z0-9 ]+$/')]
     public $o_e = '';
 
-    #[Validate('regex:/^[A-Za-z0-9 ]+$/')]
+    #[Validate('nullable|regex:/^[A-Za-z0-9 ]+$/')]
     public $pr = '';
 
-    #[Validate('regex:/^[A-Za-z0-9. ]+$/')]
+    #[Validate('nullable|regex:/^[A-Za-z0-9. ]+$/')]
     public $temperature = '';
 
-    #[Validate('regex:/^[A-Za-z0-9 ]+$/')]
+    #[Validate('nullable|regex:/^[A-Za-z0-9 ]+$/')]
     public $bp_systolic = '';
 
-    #[Validate('regex:/^[A-Za-z0-9 ]+$/')]
+    #[Validate('nullable|regex:/^[A-Za-z0-9 ]+$/')]
     public $bp_diastolic = '';
 
-    #[Validate('regex:/^[A-Za-z0-9 ]+$/')]
+    #[Validate('nullable|regex:/^[A-Za-z0-9 ]+$/')]
     public $cvs = '';
 
-    #[Validate('regex:/^[A-Za-z0-9 ]+$/')]
+    #[Validate('nullable|regex:/^[A-Za-z0-9 ]+$/')]
     public $p_a = '';
 
-    #[Validate('regex:/^[A-Za-z0-9 ]+$/')]
+    #[Validate('nullable|regex:/^[A-Za-z0-9 ]+$/')]
     public $cns = '';
 
-    #[Validate('regex:/^[A-Za-z0-9 ]+$/')]
+    #[Validate('nullable|regex:/^[A-Za-z0-9 ]+$/')]
     public $cbc = '';
 
-    #[Validate('regex:/^[A-Za-z0-9 ]+$/')]
+    #[Validate('nullable|regex:/^[A-Za-z0-9 ]+$/')]
     public $esr = '';
 
-    #[Validate('regex:/^[A-Za-z0-9 ]+$/')]
+    #[Validate('nullable|regex:/^[A-Za-z0-9 ]+$/')]
     public $crp = '';
 
-    #[Validate('regex:/^[A-Za-z0-9 ]+$/')]
+    #[Validate('nullable|regex:/^[A-Za-z0-9 ]+$/')]
     public $rft = '';
 
-    #[Validate('regex:/^[A-Za-z0-9 ]+$/')]
+    #[Validate('nullable|regex:/^[A-Za-z0-9 ]+$/')]
     public $lft = '';
 
-    #[Validate('regex:/^[A-Za-z0-9 ]+$/')]
+    #[Validate('nullable|regex:/^[A-Za-z0-9 ]+$/')]
     public $clotting_time = '';
 
-    #[Validate('regex:/^[A-Za-z0-9 ]+$/')]
+    #[Validate('nullable|regex:/^[A-Za-z0-9 ]+$/')]
     public $bleeding_time = '';
 
-    #[Validate('regex:/^[A-Za-z0-9 ]+$/')]
+    #[Validate('nullable|regex:/^[A-Za-z0-9 ]+$/')]
     public $prothrombin_time = '';
 
-    #[Validate('regex:/^[A-Za-z0-9 ]+$/')]
+    #[Validate('nullable|regex:/^[A-Za-z0-9 ]+$/')]
     public $procalcitonin = '';
 
-    #[Validate('regex:/^[A-Za-z0-9 ]+$/')]
+    #[Validate('nullable|regex:/^[A-Za-z0-9 ]+$/')]
     public $laboratory_report_file = '';
 
     #[Validate('regex:/^[A-Za-z0-9 ]+$/')]
