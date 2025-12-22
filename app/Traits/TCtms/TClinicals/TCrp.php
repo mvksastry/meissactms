044a838cee8159a96c51b9bd8f4438df966d51b7
mvksastry <mvksastry@gmail.com>
2025-12-22 17:30:23 +0530

    Traits for new clinical data addition done

---
+++ app/Traits/TCtms/TClinicals/TCrp.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Traits\TCtms\TClinicals;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+//Uuid import class
+use Illuminate\Support\Str;
+
+use File;
+
+//Models
+use App\Models\Ctms\Clinicals\Crp;
+
+use Illuminate\Support\Facades\Log;
+
+trait TCrp
+{
+
+
+}
