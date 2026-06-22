7ee3e65c11af32fd4d57ebb02b59c5c46a8df2fd
mvksastry <mvksastry@gmail.com>
2026-06-22 13:48:45 +0530

    Graphic display of patients clinical data being done as a test.

--- resources/views/livewire/charts/clinical-data-chart.blade.php
+++ resources/views/livewire/charts/clinical-data-chart.blade.php
@@ -23,29 +23,69 @@
 </div>
 @assets
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://www.chartjs.org/samples/2.9.4/utils.js"></script>
 @endassets
 @script
     <script>
     const ctx = document.getElementById('barChart');
 
+const data = [{x: 1, y: 13}, 
+              {x: 1, y: 16},
+              {x: 2, y: 16}, 
+              {x: 2, y: 23}];
+
     new Chart(ctx, {
-        type: 'bar',
+        type: 'scatter',
+        //labels: ['Hb', 'WBC', 'RBC', 'Baso', 'Neutrophils', 'Eisono'],
         data: {
-        labels: ['Hb', 'WBC', 'RBC', 'Baso', 'Neutrophils', 'Eisono'],
+        
+        
+        datasets: [{
+          label: 'Clinical Parameters',
+          data: [
+              {x: '0', y: 0}, 
+              {x: 'Hb', y: 13}, 
+              {x: 'Hb', y: 16},
+              {x: 'Hb', y: 15},
+              {x: 'Hb', y: 14},
+              {x: 'Eiso', y: 16}, 
+              {x: 'Eiso', y: 23},
+              {x: 'Baso', y: 2}, 
+              {x: 'Baso', y: 3}],
+          borderColor: 'rgb(0, 99, 132)',
+          backgroundColor: 'rgb(255, 99, 132)',
+        }]
+        /*
         datasets: [{
             label: '# Clinical Parameters',
             data: [12, 19, 3, 5, 2, 3],
             borderWidth: 1
         }]
+        */
         },
         options: {
-        scales: {
-            y: {
-            beginAtZero: true
+          responsive: true,
+          plugins: {
+            legend: {
+              position: 'top',
+            },
+            title: {
+              display: true,
+              text: 'Chart.js Scatter Chart'
             }
-        }
+          },
+          scales: {
+                    x: {
+              type: 'category',
+              position: 'bottom'
+              },
+              y: {
+              beginAtZero: true
+              }
+          }
         }
     });
+    
     </script>
     @endscript
 
