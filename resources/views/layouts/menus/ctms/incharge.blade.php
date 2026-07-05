d92f01b168ce9b2356a37ce472645a8f70d81cce
mvksastry <mvksastry@gmail.com>
2026-07-05 06:33:39 +0530

    The home page link name changed from Manage Inventory to Inventory Home

--- resources/views/layouts/menus/ctms/incharge.blade.php
+++ resources/views/layouts/menus/ctms/incharge.blade.php
@@ -420,7 +420,7 @@
               <li class="nav-item">
                 <a href="/manage-inventory" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Manage Inventory</p>
+                  <p>Inventory Home</p>
                 </a>
               </li>
             </ul>
