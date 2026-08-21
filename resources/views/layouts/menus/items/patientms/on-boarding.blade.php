c9309608a7ce4a0e68edd357ec6eb773739ceb42
mvksastry <mvksastry@gmail.com>
2026-08-21 17:29:24 +0530

    hyphen missing in on boarding to make it joined word

--- resources/views/layouts/menus/items/patientms/on-boarding.blade.php
+++ resources/views/layouts/menus/items/patientms/on-boarding.blade.php
@@ -1,6 +1,6 @@
 <li class="nav-item">
   <a href="/new-patient-onboarding" class="nav-link">
     <i class="far fa-circle nav-icon"></i>
-    <p>New On Boarding</p>
+    <p>New On-Boarding</p>
   </a>
 </li>
