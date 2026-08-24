59d882044f974de9e3b6d88d484846dd8cc476a4
mvksastry <mvksastry@gmail.com>
2026-08-24 14:23:31 +0530

    todo list relalted correction, table top links removed

--- resources/views/layouts/home/commons/homePageTodos.blade.php
+++ resources/views/layouts/home/commons/homePageTodos.blade.php
@@ -5,15 +5,6 @@
       <i class="ion ion-clipboard mr-1"></i>
       To Do List
     </h3>
-    <div class="card-tools">
-      <ul class="pagination pagination-sm">
-        <li class="page-item"><a href="#" class="page-link">&laquo;</a></li>
-        <li class="page-item"><a href="#" class="page-link">1</a></li>
-        <li class="page-item"><a href="#" class="page-link">2</a></li>
-        <li class="page-item"><a href="#" class="page-link">3</a></li>
-        <li class="page-item"><a href="#" class="page-link">&raquo;</a></li>
-      </ul>
-    </div>
   </div>
   <!-- /.card-header -->
   <livewire:general.todo-list>
