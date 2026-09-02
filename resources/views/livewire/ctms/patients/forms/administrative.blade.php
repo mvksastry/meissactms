88c07ad7fcb3d966e42b09f40d71f0487ded9ef0
mvksastry <mvksastry@gmail.com>
2026-09-03 05:01:16 +0530

    enrollment home, directors info of admin and transplant status opened for viewing based on stage code

--- resources/views/livewire/ctms/patients/forms/administrative.blade.php
+++ resources/views/livewire/ctms/patients/forms/administrative.blade.php
@@ -1,43 +1,89 @@
-  <table id="userIndex2" class="table table-sm table-bordered table-hover">
-    <thead>
-      <tr>
-        <th colspan="6" align="center"></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>
-          <label>Unique ID Number</label>
-          <input wire:model.defer="form_g.patient_unique_id" type="text" class="form-control"
-            placeholder="Unique ID Number">
-        </td>
-        <td>
-          <label>Linked Master Batch Record</label>
-          <input wire:model.defer="form_g.mbr_id" type="text" class="form-control" placeholder="MBR Code">
-        </td>
-        <td>
-          <label>Sample ID</label>
-          <input wire:model.defer="form_g.linked_sample_id" type="text" class="form-control" placeholder="Sample ID">
-        </td>
-      </tr>
-      <tr>
-        <td colspan="3">
-          <label>Other Infos</label>
-          <input wire:model.defer="form_g.other_infos" type="text" class="form-control" placeholder="Other Infos 1">
-        </td>
-      </tr>
-      <tr>
-        <td colspan="3">
-          <label>Comment</label>
-          <input wire:model.defer="form_g.administrative_comment" id="other_infos_2" type="text" class="form-control"
-            placeholder="Other Infos 2">
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <button wire:click="fnSaveEnrollmentIDs()" class="btn btn-info text-white font-normal mt-3 rounded">ASSIGN
-            IDS</button>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  @if ($enrObj->stage_code >= 350)
+    <table id="userIndex2" class="table table-sm table-bordered table-hover">
+      <thead>
+        <tr>
+          <th colspan="6" align="center"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <label>Unique ID Number</label>
+            </br>
+            {{ $enrObj->patient_unique_id }}
+          </td>
+          <td>
+            <label>Linked Master Batch Record</label>
+            </br>
+            {{ $enrObj->mbr_id }}
+          </td>
+          <td>
+            <label>Sample ID</label>
+            </br>
+            {{ $enrObj->linked_sample_id }}
+          </td>
+        </tr>
+        <tr>
+          <td colspan="3">
+            <label>Other Infos</label>
+            </br>
+            {{ $enrObj->other_infos }}
+          </td>
+        </tr>
+        <tr>
+          <td colspan="3">
+            <label>Comment</label>
+            </br>
+            {{ $enrObj->administrative_comment }}
+
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  @else
+    <table id="userIndex2" class="table table-sm table-bordered table-hover">
+      <thead>
+        <tr>
+          <th colspan="6" align="center"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <label>Unique ID Number</label>
+            <input wire:model.defer="form_g.patient_unique_id" type="text" class="form-control"
+              placeholder="Unique ID Number">
+          </td>
+          <td>
+            <label>Linked Master Batch Record</label>
+            <input wire:model.defer="form_g.mbr_id" type="text" class="form-control" placeholder="MBR Code">
+          </td>
+          <td>
+            <label>Sample ID</label>
+            <input wire:model.defer="form_g.linked_sample_id" type="text" class="form-control"
+              placeholder="Sample ID">
+          </td>
+        </tr>
+        <tr>
+          <td colspan="3">
+            <label>Other Infos</label>
+            <input wire:model.defer="form_g.other_infos" type="text" class="form-control"
+              placeholder="Other Infos 1">
+          </td>
+        </tr>
+        <tr>
+          <td colspan="3">
+            <label>Comment</label>
+            <input wire:model.defer="form_g.administrative_comment" id="other_infos_2" type="text"
+              class="form-control" placeholder="Other Infos 2">
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <button wire:click="fnSaveEnrollmentIDs()" class="btn btn-info text-white font-normal mt-3 rounded">ASSIGN
+              IDS</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  @endif
--- resources/views/livewire/ctms/patients/forms/transplantation.blade.php
+++ resources/views/livewire/ctms/patients/forms/transplantation.blade.php
@@ -1,55 +1,95 @@
-  <table id="userIndex2" class="table table-sm table-bordered table-hover">
-    <thead>
-      <tr>
-        <th colspan="6" align="center"></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>
-          <div class="col-sm-6">
-            <!-- radio -->
-            <div class="form-group">
-              <div class="form-check">
-                <input wire:model.defer="form_h.transplant_status" value="360" class="form-check-input" type="radio"
-                  name="radio1">
-                <label class="form-check-label">Transplantation Aborted</label>
-              </div>
-              <div class="form-check">
-                <input wire:model.defer="form_h.transplant_status" value="370" class="form-check-input"
-                  type="radio" name="radio1">
-                <label class="form-check-label">Transplantation Complete</label>
+  @if ($enrObj->stage_code >= 370)
+    <table id="userIndex2" class="table table-sm table-bordered table-hover">
+      <thead>
+        <tr>
+          <th colspan="6" align="center"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <label>Transplantation Status:</label>
+            </br>
+            {{ $enrObj->stage_code == 370 ? 'Transplantation Complete' : 'Transplantation Aborted' }}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label>Date of Transplantation:</label>
+            </br>
+            {{ $enrObj->transplantation_date }}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label>Transplant Info</label>
+            </br>
+            {{ $enrObj->transplantation_info }}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label>Transplantation Comments</label>
+            </br>
+            {{ $enrObj->transplantation_comments }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  @else
+    <table id="userIndex2" class="table table-sm table-bordered table-hover">
+      <thead>
+        <tr>
+          <th colspan="6" align="center"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <div class="col-sm-6">
+              <!-- radio -->
+              <div class="form-group">
+                <div class="form-check">
+                  <input wire:model.defer="form_h.transplant_status" value="360" class="form-check-input"
+                    type="radio" name="radio1">
+                  <label class="form-check-label">Transplantation Aborted</label>
+                </div>
+                <div class="form-check">
+                  <input wire:model.defer="form_h.transplant_status" value="370" class="form-check-input"
+                    type="radio" name="radio1">
+                  <label class="form-check-label">Transplantation Complete</label>
+                </div>
               </div>
             </div>
-          </div>
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <label>Date of Transplantation</label>
-          <input wire:model.defer="form_h.transplantation_date" type="date" class="form-control"
-            placeholder="Transplantation Date">
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <label>Transplant Info</label>
-          <input wire:model.defer="form_h.transplantation_info" type="text" class="form-control"
-            placeholder="Fitness Info">
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <label>Transplantation Comments</label>
-          <input wire:model.defer="form_h.transplantation_comments" type="text" class="form-control"
-            placeholder="Comments">
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <button wire:click="fnSaveTransplantationData()"
-            class="btn btn-success text-white font-normal mt-3 rounded">ADD TRANSPLANTATION DATA</button>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label>Date of Transplantation</label>
+            <input wire:model.defer="form_h.transplantation_date" type="date" class="form-control"
+              placeholder="Transplantation Date">
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label>Transplant Info</label>
+            <input wire:model.defer="form_h.transplantation_info" type="text" class="form-control"
+              placeholder="Fitness Info">
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label>Transplantation Comments</label>
+            <input wire:model.defer="form_h.transplantation_comments" type="text" class="form-control"
+              placeholder="Comments">
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <button wire:click="fnSaveTransplantationData()"
+              class="btn btn-success text-white font-normal mt-3 rounded">ADD TRANSPLANTATION DATA</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  @endif
